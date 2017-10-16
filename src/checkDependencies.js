657457a4dcb2f361ece84e3f7b0ebd43d1d984b3
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-10-16 15:38:04 +0200

    Add script to check dependencies on installation

---
+++ src/checkDependencies.js
@@ -0,0 +1,34 @@
+var process = require("process");
+var exec = require("child_process").exec;
+
+var version = process.version;
+var isWin = process.platform === "win32";
+
+var missingDep = "MISSING DEPENDENCY: ";
+var checkProgramInPath = function(bin) {
+  return isWin
+    ? 'powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Get-Command ' +
+        bin +
+        '.exe"'
+    : "command -v " + bin + " >/dev/null 2>&1 || { exit 1; }";
+};
+
+if (
+  version.charAt(1) < 8 ||
+  (version.charAt(1) == 8 && version.charAt(3) < 5)
+) {
+  console.error(missingDep, "Node v8.5.0 or later version is required!");
+  process.exit(1);
+}
+
+exec(checkProgramInPath("pandoc"), function(err, stdout, stderr) {
+  if (err !== null) {
+    console.error(missingDep, "Pandoc is required");
+    process.exit(1);
+  }
+});
+exec(checkProgramInPath("java"), function(err, stdout, stderr) {
+  if (err !== null) {
+    console.warn(missingDep, "Java is required for plantuml diagram");
+  }
+});
