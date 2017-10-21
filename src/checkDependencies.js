62602584e0c7cba91ae364c3cc726f7875837576
Antoine du Hamel <duhamelantoine1995@gmail.com>
2017-10-21 17:13:17 +0200

    Removing Pandoc of the checkDependencies module

--- src/checkDependencies.js
+++ src/checkDependencies.js
@@ -4,7 +4,7 @@ var exec = require("child_process").exec;
 var version = process.version;
 var isWin = process.platform === "win32";
 
-var missingDep = "MISSING DEPENDENCY: ";
+var missingDep = "MISSING DEPENDENCY:";
 var checkProgramInPath = function(bin) {
   return isWin
     ? 'powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Get-Command ' +
@@ -13,20 +13,11 @@ var checkProgramInPath = function(bin) {
     : "command -v " + bin + " >/dev/null 2>&1 || { exit 1; }";
 };
 
-if (
-  version.charAt(1) < 8 ||
-  (version.charAt(1) == 8 && version.charAt(3) < 5)
-) {
+if (version < "v8.5.0") {
   console.error(missingDep, "Node v8.5.0 or later version is required!");
   process.exit(1);
 }
 
-exec(checkProgramInPath("pandoc"), function(err, stdout, stderr) {
-  if (err !== null) {
-    console.error(missingDep, "Pandoc is required");
-    process.exit(1);
-  }
-});
 exec(checkProgramInPath("java"), function(err, stdout, stderr) {
   if (err !== null) {
     console.warn(
