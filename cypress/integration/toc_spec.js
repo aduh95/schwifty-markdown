6afd79f4243819214e85a9edc22322d2e04fd9c9
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-11-15 13:20:29 +0100

    Add tests for deepest level option

--- cypress/integration/toc_spec.js
+++ cypress/integration/toc_spec.js
@@ -46,8 +46,40 @@ describe("Test HTML rendering", function() {
       });
   });
 
+  it("Tests the TOC deeepest level option", function() {
+    cy.request(
+      Cypress.env("host") +
+        "md/" +
+        encodeURIComponent(Cypress.env("testDir") + "/toc_deepest_level.md")
+    );
+    cy
+      .visit(Cypress.env("host"))
+      .then(() => cy.get("#toc"))
+      .then($toc => {
+        let toc = $toc[0];
+        cy
+          .wrap(toc.firstElementChild.nodeName.toLowerCase())
+          .should("eq", "details");
+
+        let list = toc.firstElementChild.lastElementChild;
+        let listChildren = list.children;
+        cy.wrap(list.nodeName.toLowerCase()).should("eq", "ol");
+
+        // Main list should contain 3 items (Because doc has 3 h1 below the nav)
+        cy.wrap(listChildren.length).should("eq", 3);
+        cy.wrap(listChildren).each($item => {
+          cy
+            .wrap($item)
+            .children()
+            // .last()
+            // .children()
+            .its("length")
+            .should("eq", 1);
+        });
+      });
+  });
+
   it("Tests the i18n", function() {
-    // %D9%81%D9%87%D8%B1%D8%B3
     cy.request(
       Cypress.env("host") +
         "md/" +
---
+++ tests/toc_deepest_level.md
@@ -0,0 +1,31 @@
+# This should not be in the TOC
+
+<nav id="toc" data-deepest-level="1"></nav>
+
+# Level 1
+
+## Level 2
+
+# Level 1
+
+## Level 2
+
+### Level 3
+
+## Level 2
+
+### Level 3
+
+#### Level 4
+
+##### Level 5
+
+###### Level 6
+
+## Level 2
+
+###### Level 6
+
+<p class="tag">Some text</p>
+
+# Level 1
