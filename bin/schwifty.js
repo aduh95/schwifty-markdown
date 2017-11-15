bc51e75c0096ef8dc97288719c060bd95eee35d0
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-11-15 18:11:01 +0100

    Add test on port usage before running

--- bin/schwifty.js
+++ bin/schwifty.js
@@ -21,6 +21,14 @@ const argv = require("../src/cli-args")
     "$0 file.md",
     "Starts Schwifty server and listen to all changes on `file.md`"
   )
+  .example(
+    "$0 --no-browser file.md",
+    "Ask Schwifty not to open the default browser"
+  )
+  .example(
+    "$0 --browser=firefox file.md",
+    "Ask Schwifty to open Firefox rather than the default browser"
+  )
   .boolean("q")
   .alias("q", ["quiet", "no-output"])
   .boolean("u")
@@ -66,27 +74,42 @@ if (argv.u) {
     );
   });
 } else if (fs.existsSync(watchable)) {
+  const isPortAvailable = require("is-port-available");
+
   const FgRed = "\x1b[31m";
   const FgYellow = "\x1b[33m";
   const FgReset = "\x1b[0m";
-  fs
-    .readFile(PACKAGE_FILE)
+
+  isPortAvailable(argv.p)
+    .then(result => {
+      if (!result) {
+        console.error("Please specify another port (see help for more info)");
+        throw new Error("Bad port: " + isPortAvailable.lastError);
+      }
+    })
+    .then(() => fs.readFile(PACKAGE_FILE))
     .then(json => {
       let data = JSON.parse(json);
 
-      let options = "";
+      let options = [];
       for (let option in argv) {
-        if (option === "_" || option.length > 1 || argv[option] === false)
+        if (
+          option === "_" ||
+          option.length > 1 ||
+          argv[option] === false ||
+          argv[option] === undefined
+        )
           continue;
         try {
-          options += `-${option} ${shellescape(argv[option])}`;
+          options.push(`-${option} ${shellescape(argv[option])}`);
         } catch (e) {
           console.error(e);
           console.log(option, argv[option]);
         }
       }
+      options.push(shellescape(watchable));
 
-      let subprocess = exec(data.scripts.start + " " + shellescape(watchable), {
+      let subprocess = exec(data.scripts.start + " " + options.join(" "), {
         cwd: WORKING_DIR,
       });
 
@@ -101,7 +124,7 @@ if (argv.u) {
         subprocess.stdout.on("data", data => console.log(data.trim()));
       }
     })
-    .catch(err => console.error(err));
+    .catch(err => console.error(FgRed + err + FgReset));
 } else {
   console.error("No such file or directory");
   process.exit(1);
