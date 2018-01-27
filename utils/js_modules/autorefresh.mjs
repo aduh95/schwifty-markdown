c634d7ac6f9e614df9846b543ca7332d4c707b69
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-01-27 18:35:51 +0100

    Fix multiple tabs opening on Firefox

--- utils/js_modules/autorefresh.mjs
+++ utils/js_modules/autorefresh.mjs
@@ -1,17 +1,22 @@
 /**
  * @author aduh95
  * Really simple WebSocket client module
- * 
+ *
  * Its goal is to reload the page anytime the server send something
  * and to close it once the server closes the connection.
  */
 
 const socket = new WebSocket("ws://" + window.location.host + "/");
 
+const onClose = () => window.close();
+
 // Listen for messages to reload the page
-socket.addEventListener("message", () => window.location.reload());
+socket.addEventListener("message", () => {
+  socket.removeEventListener("close", onClose);
+  window.location.reload();
+});
 
 // When server closes the connection, let's close the tab
-socket.addEventListener("close", () => window.close());
+socket.addEventListener("close", onClose);
 
 export default socket;
