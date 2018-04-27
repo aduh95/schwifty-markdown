61068ac27e060e08509f196522bfee5888139aac
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-04-27 18:14:45 +0200

    Improve perf for fs/promises polyfill

--- src/fs-promises.js
+++ src/fs-promises.js
@@ -1,26 +1,61 @@
+// Some function of the core fs module are needed
 const { constants, watch, createReadStream } = require("fs");
 
 try {
-  if (!("finally" in Promise.prototype)) throw new Error();
+  if (!("finally" in Promise.prototype)) {
+    // Node-Chakracore embeds fs/promises but crashes depending of Promise.finally support
+    throw new Error();
+  }
   module.exports = require("fs/promises");
 } catch (e) {
   // Polyfill for Node.js not supporting fs/promises
   const fs = require("fs");
   const { promisify } = require("util");
 
+  // Methods available in fs/promises as of Node 10
+  const promisedMethods = [
+    "access",
+    "copyFile",
+    "open",
+    "read",
+    "write",
+    "rename",
+    "truncate",
+    "ftruncate",
+    "rmdir",
+    "fdatasync",
+    "fsync",
+    "mkdir",
+    "readdir",
+    "readlink",
+    "symlink",
+    "fstat",
+    "lstat",
+    "stat",
+    "link",
+    "unlink",
+    "fchmod",
+    "chmod",
+    // "lchmod", // Not promisifiable
+    // "lchown", // Not promisifiable
+    "fchown",
+    "chown",
+    "utimes",
+    "futimes",
+    "realpath",
+    "mkdtemp",
+    "writeFile",
+    "appendFile",
+    "readFile",
+  ];
+
   console.error(
     "Warning",
     "The fs/promises API is not supported by this version of Node"
   );
 
-  module.exports = Object.keys(fs).reduce((fsPromises, methodName) => {
-    if (!methodName.endsWith("Sync") && !methodName.endsWith("Stream")) {
-      try {
-        fsPromises[methodName] = promisify(fs[methodName]);
-      } catch (_) {
-        // The method cannot be promisified
-      }
-    }
+  module.exports = promisedMethods.reduce((fsPromises, methodName) => {
+    fsPromises[methodName] = promisify(fs[methodName]);
     return fsPromises;
   }, {});
 }
