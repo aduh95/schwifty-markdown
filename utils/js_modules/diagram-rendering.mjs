b23f49369d7489de8bb9ef6504b47678395c1d65
Antoine du Hamel <antoine.duhamel@schneider-electric.com>
2018-05-15 18:05:59 +0200

    Lazyload graph preview rendering

--- utils/js_modules/diagram-rendering.mjs
+++ utils/js_modules/diagram-rendering.mjs
@@ -10,7 +10,13 @@ const diagramTypes = [
     url: "/pu/",
   },
 ];
-const init = () => {
+
+/**
+ * @type {Set<HTMLElement>}
+ */
+const createdDetailsElement = new Set();
+
+const addPreviews = () => {
   for (const diagramType of diagramTypes) {
     const codes = document.querySelectorAll(
       ".sourceCode>code." + diagramType.code
@@ -18,16 +24,50 @@ const init = () => {
     for (const code of codes) {
       const details = document.createElement("details");
       const summary = document.createElement("summary");
-      const img = document.createElement("img");
-      img.src = diagramType.url + encodeURIComponent(code.textContent);
-      img.alt = diagramType.name + " diagram";
 
-      summary.appendChild(img);
+      details.addEventListener(
+        "toggle",
+        () => {
+          const img = document.createElement("img");
+          const loadingText = document.createTextNode("Loading preview...");
+
+          details.appendChild(loadingText);
+          img.alt = diagramType.name + " diagram";
+          img.src = diagramType.url + encodeURIComponent(code.textContent);
+          img.onload = () => {
+            details.replaceChild(img, loadingText);
+          };
+        },
+        { once: true }
+      );
+
+      summary.innerHTML = "<small><i>(click to preview)</i></small>";
+
+      details.appendChild(summary);
 
       code.parentNode.parentNode.insertBefore(details, code.parentNode);
-      details.appendChild(summary);
-      details.appendChild(code.parentNode);
+      createdDetailsElement.add(details);
+
+      summary.appendChild(code.parentNode);
     }
+  }
+};
+const removePreviews = () => {
+  for (const details of createdDetailsElement) {
+    details.parentNode.replaceChild(details.firstChild.lastChild, details);
+    createdDetailsElement.delete(details);
+  }
+};
+
+const togglePreviews = mediaQueryList =>
+  mediaQueryList.matches ? removePreviews() : addPreviews();
+
+const init = () => {
+  const mql = window.matchMedia("print");
+
+  if ("HTMLDetailsElement" in window) {
+    mql.addListener(togglePreviews);
+    togglePreviews(mql);
   }
 };
 
