e845dff6e571010ea3dde49068c3b98e8b38546c
Antoine du Hamel <antoine.duhamel@schneider-electric.com>
2018-05-15 18:53:41 +0200

    Optimizing highlight-worker

--- utils/js_scripts/highlight-worker.js
+++ utils/js_scripts/highlight-worker.js
@@ -2,11 +2,7 @@ importScripts(
   "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
 );
 
-onmessage = event => {
-  let data = event.data;
-  if (Array.isArray(data)) {
-    postMessage(self.hljs.highlightAuto(data.pop(), data).value);
-  } else {
-    close();
-  }
-};
+onmessage = ({ data }) =>
+  Array.isArray(data)
+    ? postMessage(self.hljs.highlightAuto(data.pop(), data).value)
+    : self.close();
