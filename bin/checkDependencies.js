0b259490b1a58afbfe0ef31dd2342268b97681c6
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-04-29 21:37:50 -0400

    Export check dependency function

--- bin/checkDependencies.js
+++ bin/checkDependencies.js
@@ -11,34 +11,43 @@ var isWin =
   (process.platform === "win32" || /^(msys|cygwin)$/.test(process.env.OSTYPE));
 
 var missingDep = "MissingDependencyWarning";
-var checkProgramInPath = function(bin) {
-  return isWin
-    ? "powershell" +
-        "-NoProfile -InputFormat None -ExecutionPolicy Bypass" +
-        '-Command "Get-Command ' +
-        bin +
-        '.exe"'
-    : "command -v " + bin + " >/dev/null 2>&1 || { exit 1; }";
-};
 
-if (version < "v10.0.0" && version < "v8.5.0" && version < "v8.10.0") {
-  process.emitWarning(
-    "Node v8.5.0 or later version is required! You are using Node '" +
-      version +
-      "', you should not except Schwifty to work as intended.",
-    missingDep
-  );
+function checkProgramInPath(bin, callback) {
+  var command = isWin
+    ? "powershell" +
+      "-NoProfile -InputFormat None -ExecutionPolicy Bypass" +
+      '-Command "Get-Command ' +
+      bin +
+      '.exe"'
+    : "command -v " + bin + " >/dev/null 2>&1 || { exit 1; }";
+
+  exec(command, function(err) {
+    callback(err === null);
+  });
 }
 
-if (!process.env.SCHWIFTY_DISABLE_JAVA) {
-  exec(checkProgramInPath("java"), function(err, stdout, stderr) {
-    if (err !== null) {
-      process.emitWarning(
-        "Java is required for plantuml diagram rendering" +
-          "(Set environment variable `SCHWIFTY_DISABLE_JAVA`" +
-          "at true to disable this warning)",
-        missingDep
-      );
-    }
-  });
+if (module.parent) {
+  module.exports = checkProgramInPath;
+} else {
+  if (version < "v10.0.0" && version < "v8.5.0" && version < "v8.10.0") {
+    process.emitWarning(
+      "Node v8.5.0 or later version is required! You are using Node '" +
+        version +
+        "', you should not except Schwifty to work as intended.",
+      missingDep
+    );
+  }
+
+  if (!process.env.SCHWIFTY_DISABLE_JAVA) {
+    checkProgramInPath("java", function(result) {
+      if (!result) {
+        process.emitWarning(
+          "Java is required for plantuml diagram rendering" +
+            "(Set environment variable `SCHWIFTY_DISABLE_JAVA`" +
+            "at true to disable this warning)",
+          missingDep
+        );
+      }
+    });
+  }
 }
