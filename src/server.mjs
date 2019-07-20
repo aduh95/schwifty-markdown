6f6147a7a5ac2c444493724648220018acb22714
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-07-19 21:40:34 -0400

    Lazy-load more modules
    express and ws are not needed when using Node API
    child_process is needed only for PDF rendering

--- src/server.mjs
+++ src/server.mjs
@@ -1,11 +1,8 @@
 import path from "path";
 
-import express from "express";
-import webSocket from "ws";
 import * as serveMedia from "./mediaHandler.mjs";
 import { getRenderedHTML } from "./md-file-to-html.mjs";
 import { CONFIG } from "./definitions.mjs";
-import child_process from "child_process";
 
 const WAIT_FOR_BROWSER_TO_OPEN = 2500;
 let waitForBrowserToOpen = null;
@@ -38,79 +35,89 @@ export const JS_MODULES = [
 ];
 export const JS_SCRIPTS = ["/highlight-worker.js", "/papaparse.min.js"];
 
-const app = express();
-
-app.get("/", function(req, res) {
-  const html = getRenderedHTML();
-  if (html) {
-    res.header("Content-Type", "text/html").send(html);
-  } else {
-    res
-      .status(503)
-      .send(
-        "<script type=module src='" +
-          AUTO_REFRESH_MODULE +
-          "'></script><p>No markdown modified</p>"
-      );
-  }
-});
-
-for (const serverFile of [
-  {
-    type: "text/css",
-    files: CSS_FILES,
-    folder: "css",
-  },
-  {
-    type: "application/javascript",
-    files: JS_MODULES,
-    folder: "js_modules",
-  },
-  {
-    type: "application/javascript",
-    files: JS_SCRIPTS.concat(JS_NO_MODULES_FALLBACK),
-    folder: "js_scripts",
-  },
-]) {
-  for (const file of serverFile.files) {
-    app.get(
-      file,
-      serveMedia.serverFile(
-        path.join(SERVED_FILES_FOLDER, serverFile.folder + file),
-        serverFile.type
-      )
-    );
-  }
-}
-
 export const MEDIA_GET_URL = "/media/";
-app.get(MEDIA_GET_URL + ":media", serveMedia.localFile());
 export const YUML_GET_URL = "/yuml/";
-app.get(YUML_GET_URL + ":media", serveMedia.yuml());
 export const PLANTUML_GET_URL = "/pu/";
-app.get(PLANTUML_GET_URL + ":media", serveMedia.plantuml());
 export const MARKDOWN_GET_URL = "/md/";
-app.get(MARKDOWN_GET_URL + ":media", serveMedia.markdown());
+
+const createServer = express => {
+  const app = express();
+
+  app.get("/", function(req, res) {
+    const html = getRenderedHTML();
+    if (html) {
+      res.header("Content-Type", "text/html").send(html);
+    } else {
+      res
+        .status(503)
+        .send(
+          "<script type=module src='" +
+            AUTO_REFRESH_MODULE +
+            "'></script><p>No markdown modified</p>"
+        );
+    }
+  });
+
+  for (const serverFile of [
+    {
+      type: "text/css",
+      files: CSS_FILES,
+      folder: "css",
+    },
+    {
+      type: "application/javascript",
+      files: JS_MODULES,
+      folder: "js_modules",
+    },
+    {
+      type: "application/javascript",
+      files: JS_SCRIPTS.concat(JS_NO_MODULES_FALLBACK),
+      folder: "js_scripts",
+    },
+  ]) {
+    for (const file of serverFile.files) {
+      app.get(
+        file,
+        serveMedia.serverFile(
+          path.join(SERVED_FILES_FOLDER, serverFile.folder + file),
+          serverFile.type
+        )
+      );
+    }
+  }
+  app.get(MEDIA_GET_URL + ":media", serveMedia.localFile());
+  app.get(YUML_GET_URL + ":media", serveMedia.yuml());
+  app.get(PLANTUML_GET_URL + ":media", serveMedia.plantuml());
+  app.get(MARKDOWN_GET_URL + ":media", serveMedia.markdown());
+
+  return app;
+};
 
 let wsConnection = null;
 
-export const startServer = () => {
-  const server = app.listen(
-    CONFIG.getItem("PORT_NUMBER"),
-    "localhost",
-    function() {
-      console.log(
-        `Server started on http://localhost:${CONFIG.getItem("PORT_NUMBER")}`
+export const startServer = () =>
+  Promise.all([import("express"), import("ws")])
+    .then(_ => _.map(module => module.default))
+    .then(([express, { Server }]) => {
+      const server = createServer(express).listen(
+        CONFIG.getItem("PORT_NUMBER"),
+        "localhost",
+        function() {
+          console.log(
+            `Server started on http://localhost:${CONFIG.getItem(
+              "PORT_NUMBER"
+            )}`
+          );
+        }
       );
-    }
-  );
-  new webSocket.Server({ server }).on("connection", connection => {
-    wsConnection && wsConnection.close();
-    wsConnection = connection;
 
-    connection.ping(1);
-  });
-};
+      new Server({ server }).on("connection", connection => {
+        wsConnection && wsConnection.close();
+        wsConnection = connection;
+
+        connection.ping(1);
+      });
+    });
 
 export const refreshBrowser = () => {
   const OPEN = 1;
@@ -133,21 +140,23 @@ export const refreshBrowser = () => {
       .catch(console.error);
   } else if (CONFIG.getItem("PRINT_TO_PDF")) {
     console.log("Generating PDF " + CONFIG.getItem("PRINT_TO_PDF"));
-    try {
-      child_process
-        .spawn(CONFIG.getItem("BROWSER_NAME"), [
-          "--headless",
-          "--print-to-pdf=" + CONFIG.getItem("PRINT_TO_PDF"),
-          "http://localhost:" + CONFIG.getItem("PORT_NUMBER"),
-        ])
-        .on("close", function(errCode) {
-          console.log("Browser has closed, closing Schwifty...");
-          process.exit(errCode);
-        });
-    } catch (err) {
-      console.error("Have you forgot to specify the browser to use?", err);
-      process.exit(1);
-    }
+    import("child_process")
+      .then(module => module.default)
+      .then(({ spawn }) => {
+        try {
+          spawn(CONFIG.getItem("BROWSER_NAME"), [
+            "--headless",
+            "--print-to-pdf=" + CONFIG.getItem("PRINT_TO_PDF"),
+            "http://localhost:" + CONFIG.getItem("PORT_NUMBER"),
+          ]).on("close", function(errCode) {
+            console.log("Browser has closed, closing Schwifty...");
+            process.exit(errCode);
+          });
+        } catch (err) {
+          console.error("Have you forgot to specify the browser to use?", err);
+          process.exit(1);
+        }
+      });
   } else {
     console.log("Document ready");
   }
