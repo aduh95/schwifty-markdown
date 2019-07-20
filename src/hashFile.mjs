f9460a7e16c554b6e434032e1bfd99347afda112
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-07-19 21:19:12 -0400

    Lazy load crypto module

--- src/hashFile.mjs
+++ src/hashFile.mjs
@@ -1,23 +1,27 @@
 import fs from "./fs-promises.js";
-import crypto from "crypto";
+
+const HASH_ALGORITHM = "sha1";
+const HASH_OUTPUT = "hex";
 
 export default fileOrString =>
-  new Promise((resolve, reject) => {
-    const hash = crypto.createHash("sha1");
+  import("crypto")
+    .then(module => module.default)
+    .then(({ createHash }) => createHash(HASH_ALGORITHM))
+    .then(hash =>
+      fs
+        .access(fileOrString, fs.constants.R_OK)
+        .then(() => {
+          const input = fs.createReadStream(fileOrString);
 
-    fs.access(fileOrString, fs.constants.R_OK)
-      .then(() => {
-        const input = fs.createReadStream(fileOrString);
-
-        input.on("error", err => reject(err));
-        input.on("data", data => hash.update(data));
-        input.on("end", () => {
-          resolve(hash.digest("hex"));
-        });
-      })
-      .catch(() => {
-        // Plain string handling
-        hash.update(fileOrString);
-        resolve(hash.digest("hex"));
-      });
-  });
+          input.on("error", err => reject(err));
+          input.on("data", data => hash.update(data));
+          input.on("end", () => {
+            resolve(hash.digest(HASH_OUTPUT));
+          });
+        })
+        .catch(() => {
+          // Plain string handling
+          hash.update(fileOrString);
+          resolve(hash.digest(HASH_OUTPUT));
+        })
+    );
